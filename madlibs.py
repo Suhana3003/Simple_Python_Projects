c96f5d91d4682ae8a317f77cd6ec94b9f674a70f
Suhana3003 <100698761+Suhana3003@users.noreply.github.com>
2022-06-19 16:16:36 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,6 @@
+adj = input("Adjective: ").lower()
+verb1 = input("Verb: ").lower()
+verb2 = input("Verb2: ").lower()
+famper = input("Famous Personality: ").title()
+
+print("Computer programming is so "+adj+"! It makes me so excited all the time because I love to "+verb1+"! Stay hydrated and "+verb2+ " like you are "+famper+".")
